68d5cd51bc021c52499718335d84e6050ebb3edc
parikshith078 <palegar.parikshith@gmail.com>
2024-03-24 16:42:37 +0530

    Initial Commit

---
+++ Wakatimer/WakatimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakatimerApp.swift
+//  Wakatimer
+//
+//  Created by Parikshith Palegar on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WakatimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
